a852dc6b3c2bbe4f63671a0dca0771b09c6b171d
manuel.m.speranza <manuel.m.speranza@accenture.com>
2020-07-02 19:10:20 +0200

    bring cv to app.

--- servlets/cv-app/src/app/app.component.ts
+++ servlets/cv-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeUrl, SafeScript, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cv-app';
+  title : string = 'cv-app';
+  
+  trustedLinkedinBadge : SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) {
+    //this.trustedLinBadgeUrl = sanitizer.bypassSecurityTrustUrl("https://it.linkedin.com/in/manuelmsperanza?trk=profile-badge");
+    this.trustedLinkedinBadge = sanitizer.bypassSecurityTrustHtml('<script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>' +
+    '<div id="linkedInBadge" class="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="manuelmsperanza">' + 
+      '<a class="LI-simple-link" href="https://it.linkedin.com/in/manuelmsperanza?trk=profile-badge">Manuel Speranza</a>' +
+   '</div>')
+  }
+
 }
