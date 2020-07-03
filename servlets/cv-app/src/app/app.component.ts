ffcbec15fc530a2e7a207bcd8254cadc1e83f60b
manuel.m.speranza <manuel.m.speranza@accenture.com>
2020-07-03 19:59:44 +0200

    fixed linkedin badge

--- servlets/cv-app/src/app/app.component.ts
+++ servlets/cv-app/src/app/app.component.ts
@@ -9,10 +9,22 @@ import { DomSanitizer, SafeUrl, SafeScript, SafeHtml } from '@angular/platform-b
 export class AppComponent {
   title : string = 'cv-app';
   
+  trustedFacebookUrl : SafeUrl;
+  trustedTwitterUrl : SafeUrl;
+  trustedInstagramUrl : SafeUrl;
+  trustedLinkedinUrl : SafeUrl;
+  trustedGithubUrl : SafeUrl;
+  trustedStackOverflowUrl : SafeUrl;
   trustedLinkedinBadge : SafeHtml;
 
   constructor(private sanitizer: DomSanitizer) {
-    //this.trustedLinBadgeUrl = sanitizer.bypassSecurityTrustUrl("https://it.linkedin.com/in/manuelmsperanza?trk=profile-badge");
+    this.trustedFacebookUrl = sanitizer.bypassSecurityTrustUrl("https://www.facebook.com/manuel.m.speranza");
+    this.trustedTwitterUrl = sanitizer.bypassSecurityTrustUrl("https://twitter.com/SperanzaManuel");
+    this.trustedInstagramUrl = sanitizer.bypassSecurityTrustUrl("https://www.instagram.com/manuel.m.speranza/");
+    this.trustedLinkedinUrl = sanitizer.bypassSecurityTrustUrl("https://www.linkedin.com/in/manuelmsperanza");
+    this.trustedGithubUrl = sanitizer.bypassSecurityTrustUrl("https://github.com/manuelmsperanza");
+    this.trustedStackOverflowUrl = sanitizer.bypassSecurityTrustUrl("https://stackoverflow.com/users/8607724/pin%c3%b6ch");
+    
     this.trustedLinkedinBadge = sanitizer.bypassSecurityTrustHtml('<script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>' +
     '<div id="linkedInBadge" class="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="manuelmsperanza">' + 
       '<a class="LI-simple-link" href="https://it.linkedin.com/in/manuelmsperanza?trk=profile-badge">Manuel Speranza</a>' +
