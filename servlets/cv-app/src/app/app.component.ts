32af697128a9a26563e0aa7737c72966f3ef450d
manuel.m.speranza <manuel.m.speranza@accenture.com>
2020-07-03 21:07:04 +0200

    add https

--- servlets/cv-app/src/app/app.component.ts
+++ servlets/cv-app/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
   trustedLinkedinUrl : SafeUrl;
   trustedGithubUrl : SafeUrl;
   trustedStackOverflowUrl : SafeUrl;
-  trustedLinkedinBadge : SafeHtml;
+  trustedLinkedinBadge : SafeUrl;
 
   constructor(private sanitizer: DomSanitizer) {
     this.trustedFacebookUrl = sanitizer.bypassSecurityTrustUrl("https://www.facebook.com/manuel.m.speranza");
@@ -24,11 +24,8 @@ export class AppComponent {
     this.trustedLinkedinUrl = sanitizer.bypassSecurityTrustUrl("https://www.linkedin.com/in/manuelmsperanza");
     this.trustedGithubUrl = sanitizer.bypassSecurityTrustUrl("https://github.com/manuelmsperanza");
     this.trustedStackOverflowUrl = sanitizer.bypassSecurityTrustUrl("https://stackoverflow.com/users/8607724/pin%c3%b6ch");
+    this.trustedLinkedinBadge = sanitizer.bypassSecurityTrustUrl("https://it.linkedin.com/in/manuelmsperanza?trk=profile-badge");
     
-    this.trustedLinkedinBadge = sanitizer.bypassSecurityTrustHtml('<script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>' +
-    '<div id="linkedInBadge" class="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="manuelmsperanza">' + 
-      '<a class="LI-simple-link" href="https://it.linkedin.com/in/manuelmsperanza?trk=profile-badge">Manuel Speranza</a>' +
-   '</div>')
   }
 
 }
