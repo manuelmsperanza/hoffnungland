29ab15f3ec7727a43059367ef98304187f4c0c95
Speranza, Manuel M <manu.speranz@gmail.com>
2020-07-06 02:08:48 +0200

    marvel film list first version

--- servlets/cv-app/src/app/app.component.ts
+++ servlets/cv-app/src/app/app.component.ts
@@ -16,6 +16,8 @@ export class AppComponent {
   trustedGithubUrl : SafeUrl;
   trustedStackOverflowUrl : SafeUrl;
   trustedLinkedinBadge : SafeUrl;
+  trustedW3CssUrl : SafeUrl;
+  trustedPositiveSSLBadge : SafeHtml;
 
   constructor(private sanitizer: DomSanitizer) {
     this.trustedFacebookUrl = sanitizer.bypassSecurityTrustUrl("https://www.facebook.com/manuel.m.speranza");
@@ -25,7 +27,6 @@ export class AppComponent {
     this.trustedGithubUrl = sanitizer.bypassSecurityTrustUrl("https://github.com/manuelmsperanza");
     this.trustedStackOverflowUrl = sanitizer.bypassSecurityTrustUrl("https://stackoverflow.com/users/8607724/pin%c3%b6ch");
     this.trustedLinkedinBadge = sanitizer.bypassSecurityTrustUrl("https://it.linkedin.com/in/manuelmsperanza?trk=profile-badge");
-    
+    this.trustedW3CssUrl = sanitizer.bypassSecurityTrustUrl("https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_templates_cv&amp;stacked=h");
   }
-
 }
---
+++ servlets/marvel-film-list-app/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'marvel-film-list-app';
+
+  defaultColDef;
+
+  columnDefs = [
+    {headerName: 'Title', field: 'title', sortable: true, filter: true},
+    {headerName: 'Season', field: 'season', sortable: true, filter: true},
+    {headerName: 'Episode #', field: 'episode', sortable: true, filter: true},
+    {headerName: 'Episode Name', field: 'episodeName', filter: true},
+    {headerName: 'Release Date', field: 'releaseDate', sortable: true, filter: true, type: 'dateColumn'}
+];
+
+  rowData : any;
+
+  constructor(private http: HttpClient) {
+    this.defaultColDef = { resizable: true };
+  }
+
+  ngOnInit() {
+     this.http.get('/assets/marvel-film-list.json')
+      .subscribe(
+        data => this.rowData = { ...data }
+      );
+      /*.pipe(
+       tap(data => this.rowData = data)
+     );*/
+  }
+
+
+}
