a4c38c16e242b50f3db7acbd354455b45cd72134
Speranza, Manuel M <manu.speranz@gmail.com>
2020-11-15 12:51:46 +0100

    update angular version

--- servlets/order-entry-app/src/app/orders/list-orders/list-orders.component.spec.ts
+++ servlets/order-entry-app/src/app/orders/list-orders/list-orders.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ListOrdersComponent } from './list-orders.component';
 
@@ -6,7 +6,7 @@ describe('ListOrdersComponent', () => {
   let component: ListOrdersComponent;
   let fixture: ComponentFixture<ListOrdersComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ListOrdersComponent ]
     })
