db54c027b5b29dd6e67e278440e09c093bd08dc3
manuel.m.speranza <manuel.m.speranza@accenture.com>
2020-12-04 09:14:20 +0100

    App implements actionListener for default actions. Added action on select agent combobox

--- projects/orderEntrySwing/src/main/java/com/hoffnungland/orderEntry/App.java
+++ projects/orderEntrySwing/src/main/java/com/hoffnungland/orderEntry/App.java
@@ -28,29 +28,35 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.ParameterExpression;
 import javax.persistence.criteria.Root;
 import javax.swing.JButton;
 import java.awt.Rectangle;
 import javax.swing.SwingConstants;
 import java.awt.Component;
+import java.awt.Container;
+
 import javax.swing.Box;
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 import javax.swing.Action;
 import java.awt.event.ActionListener;
+import javax.swing.DefaultComboBoxModel;
 
-public class App {
+public class App implements ActionListener {
 
 	private static final Logger logger = LogManager.getLogger(App.class);
 	private Session session;
 	private EntityManager entityManager;
-	private JFrame frame;
+	private JFrame frmOrderEntry;
 	private JComboBox<String> companyComboBox;
 	private JComboBox<String> agentComboBox;
 	private Agent agent;
@@ -60,7 +66,7 @@ public class App {
 	}
 
 	public JFrame getFrame() {
-		return frame;
+		return frmOrderEntry;
 	}
 	
 	public Agent getAgent() {
@@ -89,7 +95,7 @@ public class App {
 			public void run() {
 				try {
 					App window = new App();
-					window.frame.setVisible(true);
+					window.frmOrderEntry.setVisible(true);
 				} catch (Exception e) {
 					logger.error(e);
 					e.printStackTrace();
@@ -116,12 +122,14 @@ public class App {
 		try {
 			//this.initializeHibernate();
 			this.initializeJPA();
-	
-			frame = new JFrame();
-			frame.setBounds(100, 100, 800, 600);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			frmOrderEntry = new JFrame();
+			frmOrderEntry.setName("orderEntryFrame");
+			frmOrderEntry.setTitle("Order Entry");
+			frmOrderEntry.setBounds(100, 100, 800, 600);
+			frmOrderEntry.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			SpringLayout springLayout = new SpringLayout();
-			frame.getContentPane().setLayout(springLayout);
+			frmOrderEntry.getContentPane().setLayout(springLayout);
 			
 			JLabel companyNameLabel = new JLabel("Company Name");
 			companyNameLabel.setHorizontalAlignment(SwingConstants.TRAILING);
@@ -129,9 +137,9 @@ public class App {
 			companyNameLabel.setPreferredSize(new Dimension(90, 25));
 			companyNameLabel.setMinimumSize(new Dimension(80, 25));
 			companyNameLabel.setMaximumSize(new Dimension(100, 25));
-			springLayout.putConstraint(SpringLayout.NORTH, companyNameLabel, 10, SpringLayout.NORTH, frame.getContentPane());
-			springLayout.putConstraint(SpringLayout.WEST, companyNameLabel, 5, SpringLayout.WEST, frame.getContentPane());
-			frame.getContentPane().add(companyNameLabel);
+			springLayout.putConstraint(SpringLayout.NORTH, companyNameLabel, 10, SpringLayout.NORTH, frmOrderEntry.getContentPane());
+			springLayout.putConstraint(SpringLayout.WEST, companyNameLabel, 5, SpringLayout.WEST, frmOrderEntry.getContentPane());
+			frmOrderEntry.getContentPane().add(companyNameLabel);
 			
 			companyComboBox = new JComboBox<String>();
 			springLayout.putConstraint(SpringLayout.WEST, companyComboBox, 5, SpringLayout.EAST, companyNameLabel);
@@ -141,7 +149,7 @@ public class App {
 			companyComboBox.setMaximumSize(new Dimension(32767, 25));
 			companyComboBox.setMinimumSize(new Dimension(150, 25));
 			springLayout.putConstraint(SpringLayout.NORTH, companyComboBox, 0, SpringLayout.NORTH, companyNameLabel);
-			frame.getContentPane().add(companyComboBox);
+			frmOrderEntry.getContentPane().add(companyComboBox);
 			
 			JButton newCustomerButton = new JButton("+");
 			springLayout.putConstraint(SpringLayout.EAST, companyComboBox, -5, SpringLayout.WEST, newCustomerButton);
@@ -149,44 +157,49 @@ public class App {
 			newCustomerButton.setPreferredSize(new Dimension(41, 25));
 			newCustomerButton.setMinimumSize(new Dimension(41, 25));
 			newCustomerButton.setMaximumSize(new Dimension(41, 25));
-			frame.getContentPane().add(newCustomerButton);
+			frmOrderEntry.getContentPane().add(newCustomerButton);
 			
 			JLabel agentNameLabel = new JLabel("Agent");
-			springLayout.putConstraint(SpringLayout.WEST, agentNameLabel, 400, SpringLayout.WEST, frame.getContentPane());
+			springLayout.putConstraint(SpringLayout.WEST, agentNameLabel, 400, SpringLayout.WEST, frmOrderEntry.getContentPane());
 			springLayout.putConstraint(SpringLayout.EAST, newCustomerButton, -5, SpringLayout.WEST, agentNameLabel);
-			springLayout.putConstraint(SpringLayout.WEST, agentNameLabel, 5, SpringLayout.HORIZONTAL_CENTER, frame.getContentPane());
+			springLayout.putConstraint(SpringLayout.WEST, agentNameLabel, 5, SpringLayout.HORIZONTAL_CENTER, frmOrderEntry.getContentPane());
 			agentNameLabel.setHorizontalAlignment(SwingConstants.TRAILING);
 			agentNameLabel.setPreferredSize(new Dimension(90, 25));
 			agentNameLabel.setMinimumSize(new Dimension(80, 25));
 			agentNameLabel.setMaximumSize(new Dimension(100, 25));
 			springLayout.putConstraint(SpringLayout.NORTH, agentNameLabel, 0, SpringLayout.NORTH, companyNameLabel);
-			frame.getContentPane().add(agentNameLabel);
+			frmOrderEntry.getContentPane().add(agentNameLabel);
 			
 			agentComboBox = new JComboBox<String>();
+			agentComboBox.setName("agentComboBox");
+			agentComboBox.addActionListener(this);
+			
+			agentComboBox.setEditable(true);
 			agentNameLabel.setLabelFor(agentComboBox);
 			springLayout.putConstraint(SpringLayout.WEST, agentComboBox, 5, SpringLayout.EAST, agentNameLabel);
 			agentComboBox.setPreferredSize(new Dimension(200, 25));
 			agentComboBox.setMinimumSize(new Dimension(150, 25));
 			agentComboBox.setBorder(UIManager.getBorder("ComboBox.border"));
-			
+			agentComboBox.addItem(null);
 			this.retrieveAgents();
 			
 			springLayout.putConstraint(SpringLayout.NORTH, agentComboBox, 0, SpringLayout.NORTH, companyNameLabel);
-			frame.getContentPane().add(agentComboBox);
+			frmOrderEntry.getContentPane().add(agentComboBox);
 			
 			JButton newAgentButton = new JButton("+");
-			newAgentButton.addActionListener(new AgentDetailActionListener(this));
+			newAgentButton.setName("agentDetailButton");
+			newAgentButton.addActionListener(this);
 			springLayout.putConstraint(SpringLayout.EAST, agentComboBox, -5, SpringLayout.WEST, newAgentButton);
-			springLayout.putConstraint(SpringLayout.EAST, newAgentButton, -5, SpringLayout.EAST, frame.getContentPane());
+			springLayout.putConstraint(SpringLayout.EAST, newAgentButton, -5, SpringLayout.EAST, frmOrderEntry.getContentPane());
 			newAgentButton.setPreferredSize(new Dimension(41, 25));
 			newAgentButton.setMinimumSize(new Dimension(41, 25));
 			newAgentButton.setMaximumSize(new Dimension(41, 25));
 			springLayout.putConstraint(SpringLayout.NORTH, newAgentButton, 0, SpringLayout.NORTH, companyNameLabel);
-			frame.getContentPane().add(newAgentButton);
+			frmOrderEntry.getContentPane().add(newAgentButton);
 			
 		} catch (Exception e) {
 			logger.error(e);
-			JOptionPane.showMessageDialog(this.frame, e.getMessage(), "Exception", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(this.frmOrderEntry, e.getMessage(), "Exception", JOptionPane.ERROR_MESSAGE);
 		}
 		
 		logger.traceExit();
@@ -206,6 +219,26 @@ public class App {
 		for (Agent curAgent: agentListQuery.getResultList()) {
 			this.agentComboBox.addItem(curAgent.getUserName());
 		}
+		
+		logger.traceExit();
+	}
+	
+	public void retrieveAgent(String agentName) {
+		logger.traceEntry();
+		
+		CriteriaBuilder cb = this.entityManager.getCriteriaBuilder();
+		CriteriaQuery<Agent> q = cb.createQuery(Agent.class);
+		Root<Agent> c = q.from(Agent.class);
+		q.select(c);
+		ParameterExpression<String> p = cb.parameter(String.class);
+		q.where(cb.equal(c.get("userName"), p));
+		
+		TypedQuery<Agent> agentListQuery = this.entityManager.createQuery(q);
+		agentListQuery.setParameter(p, agentName);
+		
+		this.agent = agentListQuery.getSingleResult();
+		
+		logger.debug(this.agent);
 		
 		logger.traceExit();
 	}
@@ -281,8 +314,34 @@ public class App {
 		this.entityManager.getTransaction().commit();
 		logger.traceExit();
 	}
+	
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		logger.traceEntry();
+		
+		Container sourceContainer = (Container) event.getSource();
+		
+		logger.debug(sourceContainer.getName());
+		switch(sourceContainer.getName()) {
+		case "agentComboBox":
+			this.chooseAgent(event);
+			break;
+		case "agentDetailButton":
+			this.showAgentDetailDialog();
+			break;
+		}
+		
+		logger.traceExit();
+	}
+	
+	private void chooseAgent(ActionEvent event) {
+		logger.traceEntry();
+        String agentName = (String) this.agentComboBox.getSelectedItem();
+        this.retrieveAgent(agentName);
+		logger.traceExit();
+	}
 
-	public void saveAgent(Agent agent) {
+	private void saveAgent(Agent agent) {
 		logger.traceEntry();
 		this.agent = agent;
 		this.entityManager.getTransaction().begin();
@@ -292,5 +351,23 @@ public class App {
 		this.agentComboBox.addItem(this.agent.getUserName());
 		logger.traceExit();
 		
+	}
+
+	private void showAgentDetailDialog() {
+		logger.traceEntry();
+		
+		try {
+			AgentDetailDialog agentDetailDialog = new AgentDetailDialog(this.frmOrderEntry, this.agent);
+			agentDetailDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			agentDetailDialog.setVisible(true);
+			if("OK".equals(agentDetailDialog.getResultAction())) {
+				this.saveAgent(agentDetailDialog.getAgent());
+			}
+		} catch (Exception e) {
+			logger.error(e);
+			JOptionPane.showMessageDialog(this.frmOrderEntry, e.getMessage(), "Exception", JOptionPane.ERROR_MESSAGE);
+		}
+		
+		logger.traceExit();
 	}
 }
