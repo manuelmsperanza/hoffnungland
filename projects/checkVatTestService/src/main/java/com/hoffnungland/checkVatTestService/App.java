3e2320eef8110d3ede038a89c85c207b235a2110
manuel.m.speranza <manuel.m.speranza@accenture.com>
2020-12-24 11:14:34 +0100

    fixed webservices

--- projects/checkVatTestService/src/main/java/com/hoffnungland/checkVatTestService/App.java
+++ projects/checkVatTestService/src/main/java/com/hoffnungland/checkVatTestService/App.java
@@ -1,13 +1,35 @@
 package com.hoffnungland.checkVatTestService;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.ws.Holder;
+
+import eu.europa.ec.taxud.vies.services.checkvat.CheckVatPortType;
+import eu.europa.ec.taxud.vies.services.checkvat.CheckVatTestService;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main( String[] args )
+	{
+		CheckVatTestService checkVatTestService = new CheckVatTestService();
+		CheckVatPortType checkVatPort = checkVatTestService.getCheckVatPort();
+		Holder<String> countryCode = new Holder<String>();
+		countryCode.value = "IT";
+		Holder<String> vatNumber = new Holder<String>();
+		vatNumber.value = "06250920482";
+		Holder<XMLGregorianCalendar> requestDate = null;
+		Holder<Boolean> valid = null;
+		Holder<String> name = null;
+		Holder<String> address = null;
+		checkVatPort.checkVat(countryCode, vatNumber, requestDate, valid, name, address);
+		System.out.println(name);
+		
+
+	}
 }
