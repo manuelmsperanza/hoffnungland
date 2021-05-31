ee8eb1e41c3dc53bae5e5e0ca850f40762452688
Speranza, Manuel M <manuel.speranza@webresults.it>
2021-05-31 16:08:20 +0200

    marvel-film-list-app: add Progressive Web App

--- servlets/marvel-film-list-app/src/app/app.module.ts
+++ servlets/marvel-film-list-app/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { AuthModule } from '@auth0/auth0-angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,13 @@ import {MatButtonModule} from '@angular/material/button';
       clientId: 'oGkoTO7htixNRqf8o0iY6EElZuoMMdEj'
     }),
     MatButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
