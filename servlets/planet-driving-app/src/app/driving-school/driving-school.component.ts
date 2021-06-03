c95625cfc8ed2b6e77c981c68ec943454b6388dd
Speranza, Manuel M <manuel.speranza@webresults.it>
2021-06-03 20:29:11 +0200

    add update and delete driving schools

--- servlets/planet-driving-app/src/app/driving-school/driving-school.component.ts
+++ servlets/planet-driving-app/src/app/driving-school/driving-school.component.ts
@@ -33,4 +33,42 @@ export class DrivingSchoolComponent implements OnInit {
     });
   }
 
+  saveDrivingSchool(drivingSchool : DrivingSchool) {
+    this.http.put<DrivingSchool>(environment.drivingSchoolPath, drivingSchool).pipe()
+    .subscribe(element => this.updateDrivingSchools(element));
+  }
+
+  updateDrivingSchools(drivingSchool : DrivingSchool){
+    let spliceStartIndex : number = -1;
+    this.DRIVINGSCHOOLS.forEach((element, index) => {
+      if(element.id === drivingSchool.id){
+        spliceStartIndex = index;
+      }
+    })
+
+    if(spliceStartIndex !== -1){
+      this.DRIVINGSCHOOLS.splice(spliceStartIndex, 1, drivingSchool);
+    }
+
+  }
+
+  deleteDrivingSchool(drivingSchool : DrivingSchool){
+    this.http.delete<DrivingSchool>(environment.drivingSchoolPath + '/' + drivingSchool.id).pipe()
+    .subscribe(element => this.removeItemFromDrivingSchools(drivingSchool));
+
+  }
+
+  removeItemFromDrivingSchools(drivingSchool : DrivingSchool) {
+    let spliceStartIndex : number = -1;
+    this.DRIVINGSCHOOLS.forEach((element, index) => {
+      if(element.id === drivingSchool.id){
+        spliceStartIndex = index;
+      }
+    })
+
+    if(spliceStartIndex !== -1){
+      this.DRIVINGSCHOOLS.splice(spliceStartIndex, 1);
+    }
+  }
+
 }
