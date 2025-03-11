89140f4e24dc3d994cd5e0baded48a21c43c81d1
manuelmsperanza <manu.speranz@gmail.com>
2025-03-11 02:38:24 +0100

    add material grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { ChatComponent } from './chat/chat.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 // Import the AuthService type from the SDK
 import { AuthService } from '@auth0/auth0-angular';
@@ -12,7 +13,7 @@ import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContactFormComponent, MatToolbarModule, MatButtonModule, MatIconModule, ChatComponent],
+  imports: [RouterOutlet, ContactFormComponent, MatGridListModule, MatToolbarModule, MatButtonModule, MatIconModule, ChatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
