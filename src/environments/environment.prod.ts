162c90f89fc09bae81d56d9c3d848717bbeb474c
manuelmsperanza <manu.speranz@gmail.com>
2025-03-27 17:45:34 +0100

    fixed environement configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
 import config from '../../auth_config_prod.json';
 
-const { domain, clientId, audience, apiUri, errorPath } = config as {
+const { domain, clientId, authorizationParams: { audience }, apiUri, errorPath } = config as {
   domain: string;
   clientId: string;
-  audience?: string;
+  authorizationParams: { 
+    audience?: string;
+  };
   apiUri: string;
   errorPath: string;
 };
@@ -16,7 +18,7 @@ export const environment = {
     authorizationParams: {
       ...(audience && audience !== 'YOUR_API_IDENTIFIER' ? { audience } : null),
     },
-    redirectUri: window.location.origin,
+    redirect_uri: window.location.origin,
     errorPath,
   },
   httpInterceptor: {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ import config from '../../auth_config.json';
 const { domain, clientId, authorizationParams: { audience }, apiUri, errorPath } = config as {
   domain: string;
   clientId: string;
-  authorizationParams: {
+  authorizationParams: { 
     audience?: string;
-  },
+  };
   apiUri: string;
   errorPath: string;
 };
